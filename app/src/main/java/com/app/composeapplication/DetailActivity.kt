6e68815c533167bad51500193448297423b73742
Mansi <mansi.kothari@bacancy.com>
2022-03-06 15:13:33 +0530

    Added new detail screen State listing UI changes Added navigation dependency

---
+++ app/src/main/java/com/app/composeapplication/DetailActivity.kt
@@ -0,0 +1,43 @@
+package com.app.composeapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.app.composeapplication.ui.theme.ComposeApplicationTheme
+
+class DetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeApplicationTheme {
+                CityDetails()
+            }
+        }
+    }
+}
+
+@Composable
+private fun CityDetails(){
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text("City Name")
+            Text("State")
+            Text("Country")
+            Text("Pin code")
+        }
+    }
+
+}
--- app/src/main/java/com/app/composeapplication/MainActivity.kt
+++ app/src/main/java/com/app/composeapplication/MainActivity.kt
@@ -7,12 +7,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.app.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,60 +28,81 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeApplicationTheme {
-                MyApp()
+                EntryPoint()
             }
         }
     }
 }
 
 @Composable
-private fun MyApp() {
-    // A surface container using the 'background' color from the theme
-    /*Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
-    }*/
-
+private fun EntryPoint() {
+    val navController = rememberNavController()
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
-        Greetings()
+        StateList()
     }
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
+private fun StateList(names: List<String> = List(1000) { "$it" }) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "State List") }
+            )
+        },
+        content = {
+            LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+                items(items = names) { name ->
+                    SingleState(name = name)
+                }
+            }
         }
-    }
+    )
 }
 
 @Composable
-private fun Greeting(name: String) {
-
+private fun SingleState(name: String) {
+    val context = LocalContext.current
     val expanded = rememberSaveable { mutableStateOf(false) }
 
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
-        color = MaterialTheme.colors.primary,
+        color = Color.DarkGray,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding),
             ) {
-                Text(text = "City $name")
-//                Text(text = name)
+                Text(
+                    text = "State $name",
+                    color = Color.White,
+                    /*Modifier.clickable(
+                        onClick = {
+                            Toast.makeText(context, "OnClick", Toast.LENGTH_LONG).show()
+                        }
+                    )*/
+                )
             }
-            OutlinedButton(
+            /*OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
             ) {
                 Text(if (expanded.value) "Show less" else "Show more")
+            }*/
+            IconButton(modifier = Modifier.then(Modifier.size(24.dp)),
+                onClick = { }) {
+                Icon(
+                    Icons.Filled.ArrowForward,
+                    "contentDescription",
+                    tint = Color.White
+                )
             }
         }
     }
@@ -88,7 +117,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcome to the United Kingdom!")
+            Text("Welcome to the United Kingdom!", color = MaterialTheme.colors.primary)
             Button(
                 modifier = Modifier
                     .padding(vertical = 24.dp),
@@ -99,6 +128,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
         }
     }
 }
+
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
@@ -112,6 +142,6 @@ fun OnboardingPreview() {
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
-        Greeting("Android")
+        SingleState("Amsterdam")
     }
 }
